04f7de0eb7112596d0e08325e535bf0c23f3c347
MAHIR CHOWDHURY <parvezahmmedmahir@gmail.com>
2025-11-21 20:28:13 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,10 +12,10 @@ export default defineConfig(({ mode }) => {
       plugins: [react()],
       define: {
         // Fallback to empty string to ensure replacement happens even if env var is missing
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY || ''),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || ''),
-        'process.env.VITE_SUPABASE_URL': JSON.stringify(env.VITE_SUPABASE_URL || ''),
-        'process.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(env.VITE_SUPABASE_ANON_KEY || ''),
+        'process.env.API_KEY': JSON.stringify(env.AIzaSyAwgetKf4jEM6xd8V_bzDiB1jXbAPJOAzg || ''),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.AIzaSyBzu3LX9gsKcmC1oZdUEbvygRYLhjvrA9A || ''),
+        'process.env.VITE_SUPABASE_URL': JSON.stringify(env.VITE_SUPABASE_URL=https://dsrufuqpdwonwxxwmcgd.supabase.co|| ''),
+        'process.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(env.VITE_SUPABASE_ANON_KEY=sb_publishable_qrlKQYJiZi3g79okc67Eog_rGypB_B5 || ''),
         // Polyfill process.env to prevent crash if accessed directly
         'process.env': {}
       },
